e0e8b03ed30d3d2161592aa9211e9b2ecfffeb47
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-12-24 18:43:51 +0000

    Initial commit

---
+++ CommentingServer/src/main/java/org/stacspics/rest/MLHelloWorld.java
@@ -0,0 +1,25 @@
+package org.stacspics.rest;
+
+
+
+//package org.stacspics.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+@Path("/mlhelloworld")
+public class MLHelloWorld {
+
+  @GET
+  @Path("{lang}")
+  @Produces("text/plain")
+  public String getMessage(@PathParam("lang") String lang) {
+    switch (lang.toLowerCase()) {
+      case "en": return "Hello World!";
+      case "de": return "Hallo Welt!";
+      default: return "language "+lang+" not supported.";
+    }
+  }
+}
