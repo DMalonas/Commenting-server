958383d06d400321c42e0ef315b607718b729d64
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-12-25 03:15:29 +0000

    Completing previous commit

--- CommentingServer/src/main/java/utilities/MyUtilitiesClass.java
+++ CommentingServer/src/main/java/utilities/MyUtilitiesClass.java
@@ -1,5 +1,13 @@
 package utilities;
 
+import application.PhotosRepository;
+import application.UsersPopulation;
+
 public class MyUtilitiesClass {
+
+	public static void createInitialPopulations(PhotosRepository photosRepository, UsersPopulation usersPopulation) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
